ba35e0ef8880c868bb41a7bbce3be1734287e604
Dawid Baczak <dawid.banczak99@gmail.com>
2023-08-22 20:02:17 +0200

    Added muscleParts endpoints

---
+++ backend/src/FitPlanBuddy.Application/Features/MuscleParts/Queries/GetAll/GetAllMusclePartsHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using FitPlanBuddy.Application.Dto.MusclePartDto;
+using FitPlanBuddy.Domain.Models;
+using FitPlanBuddy.Domain.Repositories;
+using MediatR;
+
+namespace FitPlanBuddy.Application.Features.MuscleParts.Queries.GetAll
+{
+    public class GetAllMusclePartsHandler : IRequestHandler<GetAllMusclePartsRequest, IEnumerable<MusclePartRead>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GetAllMusclePartsHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<MusclePartRead>> Handle(GetAllMusclePartsRequest request, CancellationToken cancellationToken)
+        {
+            var repo = await _unitOfWork.GetGenericRepositoryAsync<MusclePart>();
+            var muscleParts = await repo.GetAll();
+            return _mapper.Map<IEnumerable<MusclePartRead>>(muscleParts);
+        }
+    }
+}
